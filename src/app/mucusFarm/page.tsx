d548f15861ca68bc9f73bc55f4d230444b1e9507
0xNahhh <0xNahhh@gmail.com>
2023-07-19 20:29:10 +0900

    chore: push to redeploy

--- src/app/mucusFarm/page.tsx
+++ src/app/mucusFarm/page.tsx
@@ -1,19 +1,19 @@
-import Image from "next/image"
+// import Image from "next/image"
 
-// components
-import Header from "@/components/Header"
-import { Container } from "@/components/Container"
-import Quantity from "./Quantity"
-import Support from "./Support"
-import Collection from "./Collection"
+// // components
+// import Header from "@/components/Header"
+// import { Container } from "@/components/Container"
+// import Quantity from "./Quantity"
+// import Support from "./Support"
+// import Collection from "./Collection"
 
-// images
-import fndTrading from "@/images/fnd-trading.png"
+// // images
+// import fndTrading from "@/images/fnd-trading.png"
 
-const MucusFarm = () => {
+export default function MucusFarm() {
   return (
     <section className='relative min-h-screen flex flex-col bg-mc-brown-200 px-3 py-6 xl:px-8 xl:py-8 2xl:px-10 2xl:py-10 overflow-hidden'>
-      <Header/>
+      {/* <Header/>
       <Container className='flex-grow flex items-end mx-0 mr-auto mt-6'>
         <div className='z-10 flex flex-col gap-y-4 2xl:gap-y-6 w-[65vw] xl:w-[60vw] 2xl:w-[50vw] mx-0'>
           <div className='flex gap-x-10'>
@@ -33,9 +33,7 @@ const MucusFarm = () => {
           unoptimized
           priority
         />
-      </div>
+      </div> */}
     </section>
   )
 }
-
-export default MucusFarm
